4a9b721ec9051a413335814948a7af25635a09c2
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-04-11 14:34:38 -0400

    Header/Button components

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+
+const Header = (props) => {
+
+  const onClick = () => {
+    console.log('clicked');
+  }
+
+  return (
+    <header className="header">
+      <h1>{props.title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+export default Header;
